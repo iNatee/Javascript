a7a70121f718d33bb881cd6d81672223bac4e9f5
Nathan <57403690+iNateeM@users.noreply.github.com>
2020-02-10 10:21:07 +0000

    Update 1

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+var helloHead = document.getElementById('head1');
+
+var helloNewHead = "Hello World";
+
+helloHead.innerHTML = '<h1 id="head1">' + helloNewHead + '</h1>';
